dc126440607e31da6db8f1e2fe2306cc0a68c345
Donovan Martodikromo <donovan.dcm@gmail.com>
2017-04-13 14:44:25 +0200

    Fundamentals

---
+++ Forecasting/Alpha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecasting
+{
+    public static class Alpha
+    {
+        public static double CalculateAlpha(double[] x)
+        {
+            var sum = 0.0;
+            for (int i = 0; i < 12; i++)
+            {
+                sum = sum + x[i];
+            }
+
+            return sum / 12;
+        }
+    }
+}
